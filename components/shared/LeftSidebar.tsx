1b5a3808874a5c6bf4c40ce1840e7312d591efb5
Nihar Hegde <niharhegde163@gmail.com>
2023-10-09 18:58:53 +0530

    adding the right side bar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -14,7 +14,7 @@ const LeftSidebar = () => {
     <section
       className="
     background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col
-    justify-between overflow-y-auto p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]
+    justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]
     "
     >
       <div className="flex flex-1 flex-col gap-6 ">
