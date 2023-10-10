6ff597f2ce9569cfe453465679c3d47815e294dd
Nihar Hegde <niharhegde163@gmail.com>
2023-10-10 18:28:25 +0530

    adding mobile filters

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,7 @@
+import Filter from "@/components/shared/Filter";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 
 export default function Home() {
@@ -24,7 +26,11 @@ export default function Home() {
           placeholder="Search for questions"
           otherClasses="flex-1 "
         />
-        filter
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
       </div>
     </>
   );
