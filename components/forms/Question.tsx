9fd8e7400a14001b19591eb73801a593100c8e35
Nihar Hegde <niharhegde163@gmail.com>
2023-10-15 15:53:07 +0530

    adding basic ask question backend

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -21,6 +21,7 @@ import { Button } from "../ui/button";
 import { QuestionsSchema } from "@/lib/validations";
 import { Badge } from "../ui/badge";
 import Image from "next/image";
+import { createQuestion } from "@/lib/actions/question.action";
 
 const type: any = "create";
 
@@ -39,11 +40,12 @@ const Question = () => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof QuestionsSchema>) {
+  async function onSubmit(values: z.infer<typeof QuestionsSchema>) {
     setIsSubmitting(true);
     try {
       // *make an async call to your api to create a question
       // *containg all form data
+      await createQuestion({});
       // *navigate to home
     } catch (error) {
     } finally {
@@ -131,6 +133,8 @@ const Question = () => {
                     // @ts-ignore
                     editorRef.current = editor;
                   }}
+                  onBlur={field.onBlur}
+                  onEditorChange={(content) => field.onChange(content)}
                   initialValue=""
                   init={{
                     height: 350,
