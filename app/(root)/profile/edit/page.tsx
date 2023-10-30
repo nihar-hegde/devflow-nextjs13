86a624004ca1a1aae01306a69fe8a07b50cfd368
Nihar Hegde <niharhegde163@gmail.com>
2023-10-30 20:17:56 +0530

    edit profile half done and fixing that ask-question JSON error

--- app/(root)/profile/edit/page.tsx
+++ app/(root)/profile/edit/page.tsx
@@ -1,5 +1,4 @@
-import Question from "@/components/forms/Question";
-import { getQuestionById } from "@/lib/actions/question.action";
+import Profile from "@/components/forms/Profile";
 import { getUserById } from "@/lib/actions/user.action";
 import { ParamsProps } from "@/types";
 import { auth } from "@clerk/nextjs";
@@ -11,17 +10,12 @@ const Page = async ({ params }: ParamsProps) => {
   if (!userId) return null;
 
   const mongoUser = await getUserById({ userId });
-  const result = await getQuestionById({ questionId: params.id });
 
   return (
     <>
-      <h1 className="h1-bold text-dark100_light900">Edit Question</h1>
+      <h1 className="h1-bold text-dark100_light900">Edit Profile</h1>
       <div className="mt-9">
-        <Question
-          type={"Edit"}
-          mongoUserId={mongoUser._id}
-          questionDetails={JSON.stringify(result)}
-        />
+        <Profile clerkId={userId} user={JSON.stringify(mongoUser)} />
       </div>
     </>
   );
