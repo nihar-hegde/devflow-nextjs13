a9955df828cfe6c70370133b7ffffe81b10e522f
Nihar Hegde <niharhegde163@gmail.com>
2023-11-01 18:43:22 +0530

    added community filters

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -84,7 +84,8 @@ export async function deleteUser(params: DeleteUserParams) {
 export async function getAllUsers(params: GetAllUsersParams) {
   try {
     connectToDatabase();
-    const { searchQuery } = params;
+    const { searchQuery, filter } = params;
+
     const query: FilterQuery<typeof User> = {};
     if (searchQuery) {
       query.$or = [
@@ -92,7 +93,24 @@ export async function getAllUsers(params: GetAllUsersParams) {
         { username: { $regex: new RegExp(searchQuery, "i") } },
       ];
     }
-    const users = await User.find(query).sort({ createdAt: -1 });
+
+    let sortOptions = {};
+
+    switch (filter) {
+      case "new_users":
+        sortOptions = { joinedAt: -1 };
+        break;
+      case "old_users":
+        sortOptions = { joinedAt: 1 };
+        break;
+      case "top_contributors":
+        sortOptions = { reputation: -1 };
+        break;
+      default:
+        break;
+    }
+
+    const users = await User.find(query).sort(sortOptions);
 
     return { users };
   } catch (error) {
