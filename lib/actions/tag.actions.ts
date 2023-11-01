2378ac74263aaf5f513157160b1ed6c8798085fd
Nihar Hegde <niharhegde163@gmail.com>
2023-11-01 18:57:30 +0530

    tags filter done

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -34,15 +34,33 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
 export async function getAllTags(params: GetAllTagsParams) {
   try {
     connectToDatabase();
-    const { searchQuery } = params;
+    const { searchQuery, filter } = params;
 
     const query: FilterQuery<typeof Tag> = {};
 
     if (searchQuery) {
       query.$or = [{ name: { $regex: new RegExp(searchQuery, "i") } }];
     }
+    let sortOptions = {};
 
-    const tags = await Tag.find(query);
+    switch (filter) {
+      case "popular":
+        sortOptions = { questions: -1 };
+        break;
+      case "recent":
+        sortOptions = { createdAt: -1 };
+        break;
+      case "name":
+        sortOptions = { name: 1 };
+        break;
+      case "old":
+        sortOptions = { createdAt: 1 };
+        break;
+      default:
+        break;
+    }
+
+    const tags = await Tag.find(query).sort(sortOptions);
     // TODO complete sorting and filtering.
     return { tags };
   } catch (error) {
